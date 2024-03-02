ad7ca3954100c168b1f9f61ef395300bf0a61fd7
Alex <alex.lopez@finnovating.com>
2024-03-02 14:00:11 +0100

    Planet detail view working. Some css changes

--- src/components/planet-detail-card/PlanetDetailCard.tsx
+++ src/components/planet-detail-card/PlanetDetailCard.tsx
@@ -1,12 +1,14 @@
+import { People } from '../../interfaces/People'
 import { Planet } from '../../interfaces/Planet'
 import PlanetCircle from '../planet-circle/PlanetCircle'
 import './PlanetDetailCard.scss'
 
 interface Props {
 	planet: Planet | null
+	residents?: People[]
 }
 
-const PlanetDetailCard = ({ planet }: Props) => {
+const PlanetDetailCard = ({ planet, residents }: Props) => {
 	return (
 		<article className="detail-card">
 			<div className="detail-card__image">
@@ -14,10 +16,28 @@ const PlanetDetailCard = ({ planet }: Props) => {
 			</div>
 			<div className="detail-card__info">
 				<h1>{planet?.name}</h1>
-				<p>Diameter: <strong>{planet?.diameter}</strong></p>
-				<p>Habitants: <strong>{planet?.residents?.length}</strong></p>
-				<p>Climate: <strong>{planet?.climate}</strong></p>
-				<p>Terrain: <strong>{planet?.terrain}</strong></p>	
+				<div className='detail-card__info--list'>
+					<div>
+						<p>Diameter: <strong>{planet?.diameter}</strong></p>
+						<p>Habitants: <strong>{planet?.residents?.length}</strong></p>
+					</div>
+					<div>
+						<p>Climate: <strong>{planet?.climate}</strong></p>
+						<p>Terrain: <strong>{planet?.terrain}</strong></p>
+					</div>
+				</div>
+
+				{residents && residents.length > 0 && (
+					<>
+						<p>Residents:</p>
+						<ul className='detail-card__info__residents--list'>
+							{residents?.map(resident => (<li key={resident.name}>{resident.name}</li>))}
+						</ul>
+					</>
+				)}
+				{!residents?.length && (
+					<p>Loading residents...</p>
+				)}
 			</div>
 		</article>
 	)
--- src/pages/planet-detail/PlanetDetail.tsx
+++ src/pages/planet-detail/PlanetDetail.tsx
@@ -4,10 +4,13 @@ import { Planet } from '../../interfaces/Planet'
 import { useParams } from 'react-router-dom'
 import PlanetDetailCard from '../../components/planet-detail-card/PlanetDetailCard'
 import './PlanetDetail.scss'
+import { People } from '../../interfaces/People'
+import axios from 'axios'
 
 const PlanetDetail = () => {
 	const [planet, setPlanet] = useState<Planet | null>(null)
 	const [loading, setLoading] = useState(true)
+	const [residents, setResidents] = useState<People[]>([])
 	const { name } = useParams()
 
 	useEffect(() => {
@@ -22,11 +25,32 @@ const PlanetDetail = () => {
 		setLoading(false)
 	}, [])
 
+	useEffect(() => {
+		if(planet) {
+			getAllResidents().then(response => setResidents(response))
+		}
+	}, [planet])
+
+	const getAllResidents = async () => {
+		const allPromises = planet?.residents.map(url => axios.get(url))
+		if (allPromises) {
+			try {
+				const responses = await Promise.all(allPromises)
+				return responses.map(response => response.data) as People[]
+			} catch (error) {
+				console.error('Error al obtener información de los residentes:', error)
+				throw error
+			}
+		}
+
+		return []
+	}
+
 	if(planet) {
 		return (
 			<section>
 				{loading && <span>Loading...</span>}
-				{!loading && <PlanetDetailCard planet={planet} />}
+				{!loading && <PlanetDetailCard planet={planet} residents={residents} />}
 			</section>
 		)
 	} 
