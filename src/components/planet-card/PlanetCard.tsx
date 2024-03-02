3ad311ca1e0a2da1298cef3d70a2a2b0e58b450a
Alex <alex.lopez@finnovating.com>
2024-03-02 20:28:19 +0100

    Edit planet with form working correctly

--- src/components/planet-card/PlanetCard.tsx
+++ src/components/planet-card/PlanetCard.tsx
@@ -3,6 +3,7 @@ import { Planet } from '../../interfaces/Planet'
 import PlanetCircle from '../planet-circle/PlanetCircle'
 import './PlanetCard.scss'
 import { usePlanetStore } from '../../store/planetStore'
+import Button from '../button/Button'
 
 interface Props {
 	planet: Planet
@@ -17,6 +18,16 @@ const PlanetCard = ({planet}: Props) => {
 		navigate(`/${planet.name}`)
 	}
 
+	const handleEditClick = () => {
+		setSelectedPlanet(planet)
+		navigate(`/${planet.name}`, {state: 'edit'})
+	}
+
+	const handleDeleteClick = (e: React.MouseEvent) => {
+		e.stopPropagation()
+		console.log('Borrar')
+	}
+
 	return (
 		<article className='card' onClick={() => goToDetail()}>
 			<header>
@@ -24,11 +35,15 @@ const PlanetCard = ({planet}: Props) => {
 				<h3>{planet.name}</h3>
 			</header>
 			<div className='card__body'>
-				<p>Diameter: <strong>{planet.diameter}</strong></p>
+				<p>Diameter: <strong>{planet.diameter} km</strong></p>
 				<p>Habitants: <strong>{planet.residents.length}</strong></p>
 				<p>Climate: <strong>{planet.climate}</strong></p>
 				<p>Terrain: <strong>{planet.terrain}</strong></p>
 			</div>
+			<footer className='card__footer'>
+				<Button onClick={handleEditClick}>Editar</Button>
+				<Button onClick={handleDeleteClick}>Borrar</Button>
+			</footer>
 		</article>
 	)
 }
--- src/components/planet-detail-card/PlanetDetailCard.tsx
+++ src/components/planet-detail-card/PlanetDetailCard.tsx
@@ -1,22 +1,34 @@
 import { Planet } from '../../interfaces/Planet'
+import Button from '../button/Button'
 import PlanetCircle from '../planet-circle/PlanetCircle'
 import './PlanetDetailCard.scss'
+import {useNavigate} from 'react-router-dom'
 
 interface Props {
 	planet: Planet
 }
 
 const PlanetDetailCard = ({ planet }: Props) => {
+	const navigate = useNavigate()
+
+	const goToEdit = () => {
+		navigate(`/${planet.name}`, {state: 'edit'})
+	}
+
 	return (
 		<article className="detail-card">
 			<div className="detail-card__image">
 				<PlanetCircle width="16rem" height="16rem" terrain={planet?.terrain} />
+				<div>
+					<Button onClick={goToEdit}>Editar</Button>
+					<Button>Borrar</Button>
+				</div>
 			</div>
 			<div className="detail-card__info">
-				<h1>{planet?.name}</h1>
+				<h1 className='detail-card__info--title'>{planet?.name}</h1>
 				<div className='detail-card__info--list'>
 					<div>
-						<p>Diameter: <strong>{planet?.diameter}</strong></p>
+						<p>Diameter: <strong>{planet?.diameter} km</strong></p>
 						<p>Habitants: <strong>{planet?.residents?.length}</strong></p>
 					</div>
 					<div>
